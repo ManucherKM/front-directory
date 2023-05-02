36f23815982dab8f637afc5c8d63349f7da8f5ab
015 <015@first.local>
2023-05-02 13:05:39 +0500

    .

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import { useState } from "react"
-import ActiveInput from "./components/ActiveInput/ActiveInput"
 
 const App = () => {
-  const [value, setValue] = useState()
 
   return (
     <div>
-      <ActiveInput onChange={e => setValue(e.target.value)} value={value} />
+
     </div>
   )
 }
